fe4ce0f68e9f393a5fb7395ed18057761a00d16b
Dennis Chung <chungdennis6@gmail.com>
2019-02-23 16:51:09 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,10 +2,3 @@ def square_array(array)
   # your code here
 end
 
-numbers = [1,2,3]
-  # your code here	new_number_array = []
-end 	def square_array(numbers)
- numbers.each do |n|
-   new_number = n**2
-  new_number_array << new_number
-end 
